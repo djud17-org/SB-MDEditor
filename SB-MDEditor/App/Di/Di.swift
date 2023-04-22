8b38e943c2b9034c679f3ed538e82a49f131f9ae
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-04-22 19:57:07 +0300

    feat: add dependencies property

--- SB-MDEditor/App/Di/Di.swift
+++ SB-MDEditor/App/Di/Di.swift
@@ -3,9 +3,45 @@ import UIKit
 final class Di {
 	weak var rootVC: IRootViewController?
 	// MARK: - глобальные сервисы-зависимости
+	private let storage: UserDefaults
+	private var dependencies: AllDependencies! // swiftlint:disable:this implicitly_unwrapped_optional
 
 	// MARK: - инициализация глобальных сервисов
-	init(rootVC: IRootViewController) {
+	init(
+		rootVC: IRootViewController,
+		storage: UserDefaults = .standard
+	) {
 		self.rootVC = rootVC
+		self.storage = storage
+
+		dependencies = Dependency(
+			storage: makeFilesStorageProvider(storage)
+		)
+	}
+
+	struct Dependency: AllDependencies {
+		let storage: IFilesStorageProvider
 	}
 }
+
+// MARK: - Module Dependency
+
+protocol IMainModuleDepencency {
+	var storage: IFilesStorageProvider { get }
+}
+protocol IOpenModuleDocDepencency {
+	var storage: IFilesStorageProvider { get }
+}
+protocol ICreateModuleDocDepencency {
+	var storage: IFilesStorageProvider { get }
+}
+protocol IAboutModuleDepencency {
+	var storage: IFilesStorageProvider { get }
+}
+
+typealias AllDependencies = (
+	IMainModuleDepencency &
+	IOpenModuleDocDepencency &
+	ICreateModuleDocDepencency &
+	IAboutModuleDepencency
+)
