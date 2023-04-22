2c796224d6efe5240961592d6cf91de34cb4a371
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-04-22 20:50:16 +0300

    fix: make dependencies available for module factory

--- SB-MDEditor/App/Di/Di.swift
+++ SB-MDEditor/App/Di/Di.swift
@@ -3,6 +3,7 @@ import UIKit
 final class Di {
 	weak var rootVC: IRootViewController?
 	// MARK: - глобальные сервисы-зависимости
+
 	private let storage: UserDefaults
 	private var dependencies: AllDependencies! // swiftlint:disable:this implicitly_unwrapped_optional
 
@@ -45,3 +46,48 @@ typealias AllDependencies = (
 	ICreateModuleDocDepencency &
 	IAboutModuleDepencency
 )
+
+// MARK: - ModuleFactory
+
+extension Di: ModuleFactory {
+	func makeStartModule() -> Module {
+		makeMainModule()
+	}
+
+	func makeAboutModule() -> Module {
+		let viewController = AboutViewController()
+		let navigationVC = UINavigationController(rootViewController: viewController)
+		navigationVC.navigationBar.prefersLargeTitles = true
+		return .init(viewController: navigationVC, animatedType: .fade)
+	}
+
+	func makeMainModule() -> Module {
+		let presenter = MainPresenter()
+		let worker = MainWorker()
+		let interactor = MainInteractor(presenter: presenter, worker: worker)
+		let router = MainRouter()
+		let viewController = MainViewController(interactor: interactor, router: router, dep: dependencies)
+
+		presenter.viewController = viewController
+		router.viewController = viewController
+		router.dataStore = interactor
+		router.view = rootVC
+
+		let navigationVC = UINavigationController(rootViewController: viewController)
+		navigationVC.navigationBar.prefersLargeTitles = true
+
+		return .init(viewController: navigationVC)
+	}
+
+	func makeOpenDocModule() -> Module {
+		let viewController = OpenDocViewController()
+		let navigationVC = UINavigationController(rootViewController: viewController)
+		navigationVC.navigationBar.isHidden = true
+		return .init(viewController: navigationVC, animatedType: .dismiss)
+	}
+
+	func makeCreateDocModule() -> Module {
+		let viewController = CreateDocViewController()
+		return .init(viewController: viewController, animatedType: .fade)
+	}
+}
