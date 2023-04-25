95398d76616261e616dcfa896baacc36699c3a1d
David Tonoyan <davidtonoyan10@me.com>
2023-04-25 22:28:51 +0400

    add: File model

---
+++ SB-MDEditor/Models/File.swift
@@ -0,0 +1,88 @@
+//
+//  File.swift
+//  SB-MDEditor
+//
+//  Created by Давид Тоноян  on 25.04.2023.
+//
+
+import Foundation
+
+final class File {
+	var name: String
+	var path: String
+	var ext: String
+	var size: UInt64
+	let filetype: FileType
+	let creationDate: Date
+	var modificationDate: Date
+	var fullname: String {
+		"\(path)/\(name)"
+	}
+
+	init(
+		name: String,
+		path: String,
+		ext: String,
+		size: UInt64,
+		filetype: FileType,
+		creationDate: Date,
+		modificationDate: Date
+	) {
+		self.name = name
+		self.path = path
+		self.ext = ext
+		self.size = size
+		self.filetype = filetype
+		self.creationDate = creationDate
+		self.modificationDate = modificationDate
+	}
+}
+
+extension File {
+	enum FileType {
+		case directory
+		case file
+	}
+
+	func getFormattedSize(with size: UInt64) -> String {
+		var convertedValue = Double(size)
+		var multiplyFactor = 0
+		let tokens = ["bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+		while convertedValue > 1024 {
+			convertedValue /= 1024
+			multiplyFactor += 1
+		}
+		return String(format: "%4.2f %@", convertedValue, tokens[multiplyFactor])
+	}
+
+	func getFormattedSize() -> String {
+		getFormattedSize(with: size)
+	}
+
+	func getFormattedAttributes() -> String {
+		let formattedSize = getFormattedSize()
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateFormat = "yyyy.MM.dd HH:mm:ss"
+
+		switch filetype {
+		case .directory:
+			return "\(dateFormatter.string(from: modificationDate)) | <dir>"
+		case .file:
+			return "\"\(ext)\" – \(dateFormatter.string(from: modificationDate)) | \(formattedSize)"
+		}
+	}
+
+	func loadFileBody() -> String {
+		guard let resourcePath = Bundle.main.resourcePath else { return "" }
+
+		var text = ""
+		let fullPath = resourcePath + "/\(path)/\(name)"
+		do {
+			text = try String(contentsOfFile: fullPath, encoding: String.Encoding.utf8)
+		} catch {
+			print("Failed to read text from \(name)")
+		}
+
+		return text
+	}
+}
