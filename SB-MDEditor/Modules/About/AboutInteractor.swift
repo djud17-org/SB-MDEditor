4e5cb3cd64bf897f9eacbd370e734893c692c63c
Dmitry Churilov <98746743+chuRealOff@users.noreply.github.com>
2023-04-26 13:13:53 +0300

    feat: add AboutInteractor

---
+++ SB-MDEditor/Modules/About/AboutInteractor.swift
@@ -0,0 +1,29 @@
+//
+//  AboutInteractor.swift
+//  SB-MDEditor
+//
+//  Created by CHURILOV DMITRIY on 26.04.2023.
+//
+
+import UIKit
+
+protocol AboutBusinessLogic {
+	func doSomething(request: Main.Something.Request)
+}
+
+final class AboutInteractor: AboutBusinessLogic, MainDataStore {
+//	private let presenter: AboutPresentationLogic
+
+	init(presenter: MainPresentationLogic, worker: MainWorker) {
+//		self.presenter = presenter
+	}
+
+	// MARK: Do something
+
+	func doSomething(request: Main.Something.Request) {
+//		worker.doSomeWork()
+
+		let response = Main.Something.Response()
+//		presenter.presentSomething(response: response)
+	}
+}
